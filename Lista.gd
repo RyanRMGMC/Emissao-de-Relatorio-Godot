bf59b511ef268675a9c8cfd994bb04df5b5ef183
SWind2003 <85123800+RyanRMGMC@users.noreply.github.com>
2024-06-06 18:06:54 -0300

    Add files via upload

--- Lista.gd
+++ Lista.gd
@@ -2,34 +2,35 @@ extends Node
 
 class Person:
 	var person_name: String
-	var age: int
-	var occupation: String
+	var email: String
+	var check_in_time: String
 
-	func _init(person_name: String, age: int, occupation: String):
+	func _init(person_name: String, email: String, check_in_time: String):
 		self.person_name = person_name
-		self.age = age
-		self.occupation = occupation
+		self.email = email
+		self.check_in_time = check_in_time
 	
 	func get_data():
 		return {
 			"name": self.person_name,
-			"age": self.age,
-			"occupation": self.occupation
+			"email": self.email,
+			"check_in_time": self.check_in_time
 		}
 	
 
 func insertData():
 	var persons = []
-	persons.append(Person.new("Mike", 25, "Software Engineer"))
-	persons.append(Person.new("Steve", 30, "Doctor"))
-	persons.append(Person.new("Billy", 35, "Teacher"))
+	persons.append(Person.new("Mike", "mike123@gmail.com", "17:00"))
+	persons.append(Person.new("Steve", "stevenuniverse@hotmail.com", "16:30"))
+	persons.append(Person.new("Billy", "billyTheK1s@outlook.com", "08:00"))
+	persons.append(Person.new("Joe", "joejoe@gmail.com", "14:00"))
 	return persons
 
 #func dictionary_to_packed_string_array(dictionary):
 #	var packed_array = PackedStringArray()
 #
 #	for key in dictionary.keys():
-##		packed_array.append(str(key))
+#		packed_array.append(str(key))
 #		packed_array.append(str(dictionary[key]))
 #
 #	return packed_array
@@ -47,8 +48,9 @@ func _ready():
 		print(row)
 
 	# Save data to a CSV file
-	var file = FileAccess.open("res://ExportedCustomObjects.csv", FileAccess.WRITE)
+	var file = FileAccess.open("res://ExportedCustomObjects.csv", FileAccess.READ_WRITE)
 	if file:
+		file.store_csv_line(["Name", "E-mail", "Check-in"])
 		for row in data:
-			file.store_csv_line([row.name, str(row.age), row.occupation])
+			file.store_csv_line([row.name, row.email, row.check_in_time])
 		file.close()
